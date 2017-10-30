8dfc15df4c6f8e3484032140120bae733f22175f
mjachowdhury <study.computing2016@gmail.com>
2017-10-30 11:44:01 +0000

    Add a simple method

--- EgitExercise/src/egitExercise/Main.java
+++ EgitExercise/src/egitExercise/Main.java
@@ -7,7 +7,14 @@ public class Main {
 		SalesData data = new SalesData();
 		
 		data.display();
+		
+		displayGreeting();
 
+	}
+
+	private static void displayGreeting() {
+		 System.out.println("As salamu alykum");
+		 System.out.println("This is just Egit exercise ");
 	}
 
 }
